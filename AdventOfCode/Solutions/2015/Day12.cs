4ea9e3466e06629a1ebe2f3c8d1ad4a2b04f5dc3
EnTeeGee <enteegee@protonmail.com>
2021-08-12 23:04:18 +0100

    Days 12 and 13

---
+++ AdventOfCode/Solutions/2015/Day12.cs
@@ -0,0 +1,67 @@
+﻿using AdventOfCode.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Day12
+    {
+        [Solution(12, 1)]
+        public int Solution1(string input)
+        {
+            var jToken = JsonConvert.DeserializeObject<JToken>(input);
+
+            return GetValueOfElement(jToken);
+        }
+
+        [Solution(12, 2)]
+        public int Solution2(string input)
+        {
+            var jToken = JsonConvert.DeserializeObject<JToken>(input);
+
+            return GetValueOfElement(jToken, true);
+        }
+
+        private int GetValueOfElement(JToken element, bool skipRed = false)
+        {
+
+            if (element.Type == JTokenType.Integer)
+                return element.Value<int>();
+            
+            if(element.Type == JTokenType.Object)
+            {
+                if (skipRed)
+                {
+                    var containsRed = element.Select(it => (JProperty)it).Any(it => it.Value.Type == JTokenType.String && it.Value.ToString() == "red");
+
+                    if (containsRed)
+                        return 0;
+                }
+
+                var sum = 0;
+
+                foreach(JProperty item in element)
+                {
+                    sum+= GetValueOfElement(item.Value, skipRed);
+                }
+
+                return sum;
+            }
+
+            if(element.Type == JTokenType.Array)
+            {
+                var sum = 0;
+
+                foreach(var item in element.Value<JArray>())
+                {
+                    sum += GetValueOfElement(item, skipRed);
+                }
+
+                return sum;
+            }
+
+            return 0;
+        }
+    }
+}
