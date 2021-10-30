e7c28d1d0b58d2b3ebb5d6f8bcf2b327a4ce67bf
EnTeeGee <enteegee@protonmail.com>
2021-10-30 21:28:13 +0100

    Solutions for days 23, 24 and 25, completing the year 2015

---
+++ AdventOfCode/Solutions/2015/Day25.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Day25
+    {
+        private const int startingValue = 20151125;
+
+        [Solution(25, 1)]
+        public long Solution1(string input)
+        {
+            var items = Parser.SplitOn(input, ' ', '.', ',');
+            var row = int.Parse(items[15]);
+            var column = int.Parse(items[17]);
+
+            var priorDiag = (row + column - 2);
+            var diagStart = (priorDiag * (priorDiag + 1)) / 2;
+            var index = diagStart + column;
+
+            long value = startingValue;
+
+            for(var i = 1; i < index; i++)
+            {
+                long interim = value * 252533L;
+                value = interim % 33554393;
+            }
+
+            return value;
+        }
+    }
+}
