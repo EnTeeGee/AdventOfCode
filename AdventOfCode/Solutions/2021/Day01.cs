8f1bd8519a882e25f334a6964e3d29f06e553b38
EnTeeGee <enteegee@protonmail.com>
2021-12-01 18:15:23 +0000

    Use slightly cleaner method for day 1 part 2

--- AdventOfCode/Solutions/2021/Day01.cs
+++ AdventOfCode/Solutions/2021/Day01.cs
@@ -18,9 +18,8 @@ namespace AdventOfCode.Solutions._2021
         public int Solution2(string input)
         {
             var depths = Parser.ToArrayOfInt(input);
-            var windows = depths.Zip(depths.Skip(1), (a, b) => a + b).Zip(depths.Skip(2), (a, b) => a + b).ToArray();
 
-            return windows.Zip(windows.Skip(1), (a, b) => b - a > 0).Count(it => it);
+            return depths.Zip(depths.Skip(3), (a, b) => b - a > 0).Count(it => it);
         }
     }
 }
