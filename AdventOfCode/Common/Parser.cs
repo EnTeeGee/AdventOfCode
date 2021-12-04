3e5ba45779c62b8b07e8363a2365827b10f9394e
EnTeeGee <enteegee@protonmail.com>
2021-12-04 10:42:27 +0000

    Include split into groups method on parser

--- AdventOfCode/Common/Parser.cs
+++ AdventOfCode/Common/Parser.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode.Common
         public static T[] ToArrayOf<T>(string input, Func<string, T> converter)
         {
             return input
-                .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(it => converter.Invoke(it.Trim()))
                 .ToArray();
         }
@@ -21,6 +21,14 @@ namespace AdventOfCode.Common
         public static string[] ToArrayOfString(string input)
         {
             return ToArrayOf(input, it => it);
+        }
+
+        public static string[] ToArrayOfGroups(string input)
+        {
+            return input
+                .Split(new[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(it => it.Trim())
+                .ToArray();
         }
 
         public static string[] SplitOnSpace(string input)
