5492262bc1179d898e4f4df8184e5d76b99a2afa
EnTeeGee <enteegee@protonmail.com>
2021-12-07 22:56:45 +0000

    Day 6 solutions for 2016

---
+++ AdventOfCode/Solutions/2016/Day06.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2016
+{
+    class Day06
+    {
+        [Solution(6, 1)]
+        public string Solution1(string input)
+        {
+            var lines = Parser.ToArrayOfString(input);
+
+            return new string(Enumerable.Range(0, lines[0].Length).Select(it => lines.Select(l => l[it]).GroupBy(c => c).OrderByDescending(c => c.Count()).First().Key).ToArray());
+        }
+
+        [Solution(6, 2)]
+        public string Solution2(string input)
+        {
+            var lines = Parser.ToArrayOfString(input);
+
+            return new string(Enumerable.Range(0, lines[0].Length).Select(it => lines.Select(l => l[it]).GroupBy(c => c).OrderBy(c => c.Count()).First().Key).ToArray());
+        }
+    }
+}
