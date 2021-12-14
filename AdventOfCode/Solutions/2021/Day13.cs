47b76a99b85cb46afe5ad96db8b2aa2d4b3d93a0
EnTeeGee <enteegee@protonmail.com>
2021-12-14 08:42:23 +0000

    Solutions for days 13 and 14

---
+++ AdventOfCode/Solutions/2021/Day13.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Solutions._2021
+{
+    class Day13
+    {
+        [Solution(13, 1)]
+        public int Solution1(string input)
+        {
+            var sections = Parser.ToArrayOfGroups(input);
+            var points = Parser.ToArrayOf(sections[0], it => { var split = Parser.SplitOn(it, ','); return new Point(int.Parse(split[0]), int.Parse(split[1])); });
+            var directions = Parser.ToArrayOfString(sections[1]).Select(it => new Instruction(it)).ToArray();
+
+            return FoldPoints(points, directions[0]).Count();
+        }
+
+        [Solution(13, 2)]
+        public string Solution2(string input)
+        {
+            var sections = Parser.ToArrayOfGroups(input);
+            var points = Parser.ToArrayOf(sections[0], it => { var split = Parser.SplitOn(it, ','); return new Point(int.Parse(split[0]), int.Parse(split[1])); });
+            var directions = Parser.ToArrayOfString(sections[1]).Select(it => new Instruction(it)).ToArray();
+
+            var finalPoints = directions.Aggregate(points, (acc, i) => FoldPoints(acc, i)).ToHashSet();
+            var finalSize = new Point(finalPoints.Max(it => it.X), finalPoints.Max(it => it.Y));
+            var output = new StringBuilder();
+
+            for (var i = 0; i <= finalSize.Y; i++)
+            {
+                for(var j = 0; j <= finalSize.X; j++)
+                    output.Append(finalPoints.Contains(new Point(j, i)) ? "#" : ".");
+                output.Append(Environment.NewLine);
+            }
+
+            return output.ToString();
+        }
+
+        private Point[] FoldPoints(Point[] points, Instruction instruction)
+        {
+            if(instruction.Direction == "x")
+                return points.Select(it => new Point(it.X > instruction.Location ? instruction.Location - (it.X - instruction.Location) : it.X, it.Y)).Distinct().ToArray();
+            else
+                return points.Select(it => new Point(it.X, it.Y > instruction.Location ? instruction.Location - (it.Y - instruction.Location) : it.Y)).Distinct().ToArray();
+        }
+
+        private class Instruction
+        {
+            public string Direction { get; }
+            public int Location { get; }
+
+            public Instruction(string input)
+            {
+                var split = Parser.SplitOn(input, ' ', '=');
+                Direction = split[2];
+                Location = int.Parse(split[3]);
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/2021/Day14.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2021
+{
+    class Day14
+    {
+        [Solution(14, 1)]
+        public int Solution1(string input)
+        {
+            var chunks = Parser.ToArrayOfGroups(input);
+            var rules = Parser.ToArrayOf(chunks[1], it => Parser.SplitOnSpace(it)).ToDictionary(it => it[0], it => it[2]);
+
+            var groups = Enumerable.Range(0, 10).Aggregate(chunks[0], (acc, _) => Expand(acc, rules)).GroupBy(it => it).OrderBy(it => it.Count()).ToArray();
+
+            return groups.Last().Count() - groups.First().Count();
+        }
+
+        [Solution(14, 2)]
+        public long Solution2(string input)
+        {
+            var chunks = Parser.ToArrayOfGroups(input);
+            var rules = Parser.ToArrayOf(chunks[1], it => Parser.SplitOnSpace(it)).ToDictionary(it => it[0], it => new[] { $"{it[0][0]}{it[2]}", $"{it[2]}{it[0][1]}" });
+
+            var currentPairs = rules.Keys.ToDictionary(it => it, it => 0L);
+            foreach (var item in chunks[0].Zip(chunks[0].Skip(1), (a, b) => $"{a}{b}"))
+                currentPairs[item]++;
+
+            var currentCounts = chunks[0].GroupBy(it => it).ToDictionary(it => it.Key, it => (long)it.Count());
+
+            for(var i = 0; i < 40; i++)
+            {
+                var newPairs = currentPairs.ToDictionary(it => it.Key, it => 0L);
+                foreach(var item in currentPairs)
+                {
+                    var match = rules[item.Key];
+                    newPairs[match[0]] += item.Value;
+                    newPairs[match[1]] += item.Value;
+                    if (!currentCounts.ContainsKey(match[0][1]))
+                        currentCounts.Add(match[0][1], 0);
+                    currentCounts[match[0][1]] += item.Value;
+                }
+                currentPairs = newPairs;
+            }
+
+            var orderedCounts = currentCounts.Values.OrderBy(it => it).ToArray();
+
+            return orderedCounts.Last() - orderedCounts.First();
+        }
+
+        private string Expand(string polymer, Dictionary<string, string> mappings)
+        {
+            return string.Join(
+                string.Empty,
+                polymer.Zip(polymer.Skip(1), (a, b) => a + mappings[$"{a}{b}"]).Concat(new[] { polymer.Last().ToString() }).ToArray());
+        }
+    }
+}
