bb7c5ba834f187b6c406052c1b9dddc9b18438a8
EnTeeGee <enteegee@protonmail.com>
2021-12-25 12:02:41 +0000

    Solutions for days 24 and 25

---
+++ AdventOfCode/Solutions/2021/Day24.cs
@@ -0,0 +1,141 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2021
+{
+    class Day24
+    {
+        [Solution(24, 1)]
+        public string Solution1(string input)
+        {
+            var startingState = new Dictionary<char, int> { { 'w', 0 }, { 'x', 0 }, { 'y', 0 }, { 'z', 0 } };
+            var currentComp = new Computer(Parser.ToArrayOfString(input), startingState);
+
+            var match = GetValidNumbers(currentComp);
+
+            return string.Join(string.Empty, match);
+        }
+
+        [Solution(24, 2)]
+        public string Solution2(string input)
+        {
+            var startingState = new Dictionary<char, int> { { 'w', 0 }, { 'x', 0 }, { 'y', 0 }, { 'z', 0 } };
+            var currentComp = new Computer(Parser.ToArrayOfString(input), startingState);
+
+            var match = GetValidNumbers2(currentComp);
+
+            return string.Join(string.Empty, match);
+        }
+
+        private List<int> GetValidNumbers(Computer state)
+        {
+            for(var i = 9; i > 0; i--)
+            {
+                var tempComp = state.Clone();
+                tempComp.RunUntilNextInput(i);
+                if (!tempComp.IsWithinLimit())
+                    continue;
+
+                if (tempComp.AtEnd())
+                    return new List<int> { i };
+
+                var tail = GetValidNumbers(tempComp);
+                if (tail != null)
+                    return new List<int> { i }.Concat(tail).ToList();
+            }
+
+            return null;
+        }
+
+        private List<int> GetValidNumbers2(Computer state)
+        {
+            for (var i = 1; i < 10; i++)
+            {
+                var tempComp = state.Clone();
+                tempComp.RunUntilNextInput(i);
+                if (!tempComp.IsWithinLimit())
+                    continue;
+
+                if (tempComp.AtEnd())
+                    return new List<int> { i };
+
+                var tail = GetValidNumbers2(tempComp);
+                if (tail != null)
+                    return new List<int> { i }.Concat(tail).ToList();
+            }
+
+            return null;
+        }
+
+        private class Computer
+        {
+            public Dictionary<char, int> State { get; }
+            public string[] RemainingLines { get; private set; }
+
+            public Computer(string[] lines, Dictionary<char, int> state)
+            {
+                State = state;
+                RemainingLines = lines;
+            }
+
+            public void RunUntilNextInput(int input)
+            {
+                if (!RemainingLines[0].StartsWith("inp"))
+                    throw new Exception("Odd state");
+
+                var target = Parser.SplitOnSpace(RemainingLines[0])[1][0];
+                State[target] = input;
+                RemainingLines = RemainingLines.Skip(1).ToArray();
+
+                while (RemainingLines.Any() && !RemainingLines[0].StartsWith("inp"))
+                {
+                    var items = Parser.SplitOnSpace(RemainingLines[0]);
+                    var opTarget = items[1][0];
+                    var opSecond = items[2].Length == 1 && char.IsLetter(items[2][0]) ? State[items[2][0]] : int.Parse(items[2]);
+
+                    switch (items[0])
+                    {
+                        case "add":
+                            State[opTarget] += opSecond;
+                            break;
+                        case "mul":
+                            State[opTarget] *= opSecond;
+                            break;
+                        case "div":
+                            State[opTarget] /= opSecond;
+                            break;
+                        case "mod":
+                            State[opTarget] %= opSecond;
+                            break;
+                        case "eql":
+                            State[opTarget] = (State[opTarget] == opSecond ? 1 : 0);
+                            break;
+                    }
+
+                    RemainingLines = RemainingLines.Skip(1).ToArray();
+                }
+            }
+
+            public Computer Clone()
+            {
+                return new Computer(RemainingLines, State.ToDictionary(it => it.Key, it => it.Value));
+            }
+
+            public bool IsWithinLimit()
+            {
+                var remaining = RemainingLines.Where(it => it == "div z 26").Count();
+                var maxVal = Math.Pow(26, remaining);
+
+                return State['z'] < maxVal;
+            }
+
+            public bool AtEnd()
+            {
+                return !RemainingLines.Any();
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/2021/Day25.cs
@@ -0,0 +1,104 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions._2021
+{
+    class Day25
+    {
+        [Solution(25, 1)]
+        public int Solution1(string input)
+        {
+            var lines = Parser.ToArrayOfString(input);
+            var eastFacing = new HashSet<Point>();
+            var southFacing = new HashSet<Point>();
+            for(var i = 0; i < lines.Length; i++)
+            {
+                for(var j = 0; j < lines[0].Length; j++)
+                {
+                    var item = lines[i][j];
+                    if (item == '>')
+                        eastFacing.Add(new Point(j, i));
+                    else if (item == 'v')
+                        southFacing.Add(new Point(j, i));
+                }
+            }
+
+            var limit = new Point(lines[0].Length, lines.Length);
+            var steps = 1;
+
+            while(true)
+            {
+                var movingEast = new Dictionary<Point, Point>();
+                var movingSouth = new Dictionary<Point, Point>();
+
+                foreach(var item in eastFacing)
+                {
+                    var target = GetNext(item, limit, true);
+                    if (!eastFacing.Contains(target) && !southFacing.Contains(target))
+                        movingEast.Add(item, target);
+                }
+
+                foreach(var item in movingEast)
+                {
+                    eastFacing.Remove(item.Key);
+                    eastFacing.Add(item.Value);
+                }
+
+                foreach(var item in southFacing)
+                {
+                    var target = GetNext(item, limit, false);
+                    if (!eastFacing.Contains(target) && !southFacing.Contains(target))
+                        movingSouth.Add(item, target);
+                }
+
+                foreach(var item in movingSouth)
+                {
+                    southFacing.Remove(item.Key);
+                    southFacing.Add(item.Value);
+                }
+
+                if (!movingEast.Any() && !movingSouth.Any())
+                {
+                    //Draw(eastFacing, southFacing, limit);
+
+                    return steps;
+                }
+
+                steps++;
+            }
+        }
+
+        private Point GetNext(Point source, Point limit, bool isEast)
+        {
+            if (isEast)
+                return new Point(source.X + 1 >= limit.X ? 0 : source.X + 1, source.Y);
+            else
+                return new Point(source.X, source.Y + 1 >= limit.Y ? 0 : source.Y + 1);
+        }
+
+        private void Draw(HashSet<Point> east, HashSet<Point> south, Point limit)
+        {
+            for(var i = 0; i < limit.Y; i++)
+            {
+                var builder = new StringBuilder();
+                for(var j = 0; j < limit.X; j++)
+                {
+                    var point = new Point(j, i);
+                    if (east.Contains(point))
+                        builder.Append(">");
+                    else if (south.Contains(point))
+                        builder.Append("v");
+                    else
+                        builder.Append(".");
+                }
+
+                Console.WriteLine(builder.ToString());
+            }
+        }
+    }
+}
