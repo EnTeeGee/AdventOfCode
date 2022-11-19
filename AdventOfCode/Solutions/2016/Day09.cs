ca19c659f2757cc40f216120f7340ae915cedbad
EnTeeGee <enteegee@protonmail.com>
2022-11-19 11:51:47 +0000

    Clean up usings in days 9 and 10

--- AdventOfCode/Solutions/2016/Day09.cs
+++ AdventOfCode/Solutions/2016/Day09.cs
@@ -1,10 +1,7 @@
 ﻿using AdventOfCode.Common;
 using AdventOfCode.Core;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Solutions._2016
 {
