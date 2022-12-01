502d6ac91212e406877d50b203132a56c4d9b69e
EnTeeGee <enteegee@protonmail.com>
2022-12-01 08:59:31 +0000

    Solutions for 2022 day 1

---
+++ AdventOfCode/Solutions/2022/Day01.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Common;
+using AdventOfCode.Core;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2022
+{
+    class Day01
+    {
+        [Solution(1, 1)]
+        public int Solution1(string input)
+        {
+            return Parser.ToArrayOfGroups(input)
+                .Select(it => Parser.ToArrayOfInt(it).Sum())
+                .Max();
+        }
+
+        [Solution(1, 2)]
+        public int Solution2(string input)
+        {
+            return Parser.ToArrayOfGroups(input)
+                .Select(it => Parser.ToArrayOfInt(it).Sum())
+                .OrderByDescending(it => it)
+                .Take(3)
+                .Sum();
+        }
+    }
+}
