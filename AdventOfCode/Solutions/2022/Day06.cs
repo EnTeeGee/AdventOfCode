ebc2b972d6adaed5b4c427d434d223e58e7dafc2
EnTeeGee <enteegee@protonmail.com>
2022-12-06 08:10:55 +0000

    Solutions for day 6

---
+++ AdventOfCode/Solutions/2022/Day06.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode.Core;
+using System.Linq;
+
+namespace AdventOfCode.Solutions._2022
+{
+    class Day06
+    {
+        [Solution(6, 1)]
+        public int Solution1(string input)
+        {
+            return GetDistinctRange(input, 4);
+        }
+
+        [Solution(6, 2)]
+        public int Solution2(string input)
+        {
+            return GetDistinctRange(input, 14);
+        }
+
+        private int GetDistinctRange(string input, int range)
+        {
+            return Enumerable.Range(0, input.Length - range)
+                .Select(it => new { index = it + range, distinct = input.Skip(it).Take(range).Distinct().Count() })
+                .First(it => it.distinct == range)
+                .index;
+        }
+    }
+}
