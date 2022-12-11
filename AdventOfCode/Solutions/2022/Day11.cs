9e7ed018f0635811059a90fe9d57684ebd791615
EnTeeGee <enteegee@protonmail.com>
2022-12-11 11:53:24 +0000

    Working solution for day 11 part 2

--- AdventOfCode/Solutions/2022/Day11.cs
+++ AdventOfCode/Solutions/2022/Day11.cs
@@ -26,11 +26,14 @@ namespace AdventOfCode.Solutions._2022
         private long RunFor(string input, int steps, bool lowerWorry)
         {
             var monkeys = Parser.ToArrayOfGroups(input).Select(it => new Monkey(it)).ToArray();
+            var trim = monkeys.Aggregate(1, (acc, it) => acc * it.Test);
+            if (trim % 10 != 0)
+                trim *= 10;
+            foreach (var item in monkeys)
+                item.Trim = trim;
 
             for (var i = 0; i < steps; i++)
             {
-                Console.WriteLine("running line " + i);
-
                 foreach (var item in monkeys)
                 {
                     var result = item.ThrowAll(lowerWorry);
@@ -46,18 +49,19 @@ namespace AdventOfCode.Solutions._2022
 
         private class Monkey
         {
-            public List<BigInteger> Items { get; }
+            public List<long> Items { get; }
             public int Inspections { get; private set; }
+            public int Trim { get; set; }
+            public int Test { get; }
 
-            private Func<BigInteger, BigInteger> op;
-            private int test;
-            private int trueTarget;
-            private int falseTarget;
+            private readonly Func<long, long> op;
+            private readonly int trueTarget;
+            private readonly int falseTarget;
 
             public Monkey(string input)
             {
                 var lines = Parser.ToArrayOfString(input);
-                Items = Parser.SplitOn(lines[1], ' ', ',').Skip(2).Select(it => BigInteger.Parse(it)).ToList();
+                Items = Parser.SplitOn(lines[1], ' ', ',').Skip(2).Select(it => long.Parse(it)).ToList();
                 var opSections = Parser.SplitOnSpace(lines[2]);
                 if (opSections[3] == "old" && opSections[5] == "old")
                     op = ((it) => it * it);
@@ -65,19 +69,21 @@ namespace AdventOfCode.Solutions._2022
                     op = (it => it + int.Parse(opSections[5]));
                 else
                     op = (it => it * int.Parse(opSections[5]));
-                test = int.Parse(Parser.SplitOnSpace(lines[3])[3]);
+                Test = int.Parse(Parser.SplitOnSpace(lines[3])[3]);
                 trueTarget = int.Parse(Parser.SplitOnSpace(lines[4])[5]);
                 falseTarget = int.Parse(Parser.SplitOnSpace(lines[5])[5]);
             }
 
-            public (int target, BigInteger value)[] ThrowAll(bool lowerWorry)
+            public (int target, long value)[] ThrowAll(bool lowerWorry)
             {
-                var output = new List<(int, BigInteger)>();
+                var output = new List<(int, long)>();
 
                 foreach (var item in Items)
                 {
                     var newItem = op.Invoke(item) / (lowerWorry ? 3 : 1);
-                    if (newItem % test == 0)
+                    if (newItem >= Trim)
+                        newItem %= Trim;
+                    if (newItem % Test == 0)
                         output.Add((trueTarget, newItem));
                     else
                         output.Add((falseTarget, newItem));
