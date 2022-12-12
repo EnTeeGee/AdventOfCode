34517b0987fd4692ab05cc8619956216bdd6f19b
EnTeeGee <enteegee@protonmail.com>
2022-12-12 20:47:01 +0000

    Clean up of day 12 to remove duplication

--- AdventOfCode/Solutions/2022/Day12.cs
+++ AdventOfCode/Solutions/2022/Day12.cs
@@ -3,8 +3,6 @@ using AdventOfCode.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Solutions._2022
 {
@@ -14,14 +12,30 @@ namespace AdventOfCode.Solutions._2022
         public int Solution1(string input)
         {
             var lines = Parser.ToArrayOfString(input);
+            var (start, end, heightMap) = GenerateHeightMap(lines);
+
+            return SearchMap(start, heightMap, (height, currentHeight) => height <= currentHeight + 1, (it) => it.Equals(end));
+        }
+
+        [Solution(12, 2)]
+        public int Solution2(string input)
+        {
+            var lines = Parser.ToArrayOfString(input);
+            var (_, start, heightMap) = GenerateHeightMap(lines);
+
+            return SearchMap(start, heightMap, (height, currentHeight) => height >= currentHeight - 1, (it) => heightMap[it.X, it.Y] == 0);
+        }
+
+        private (Point start, Point end, int[,] heightMap) GenerateHeightMap(string[] input)
+        {
             var start = new Point();
             var end = new Point();
-            var heightMap = new int[lines[0].Length, lines.Length];
+            var heightMap = new int[input[0].Length, input.Length];
 
-            for(var y = 0; y < lines.Length; y++)
+            for (var y = 0; y < input.Length; y++)
             {
-                var line = lines[y];
-                for(var x = 0; x < line.Length; x++)
+                var line = input[y];
+                for (var x = 0; x < line.Length; x++)
                 {
                     var item = line[x];
                     if (item == 'S')
@@ -39,61 +53,14 @@ namespace AdventOfCode.Solutions._2022
                 }
             }
 
-            var seen = new int?[lines[0].Length, lines.Length];
-            seen[start.X, start.Y] = 0;
-            var boundary = new Queue<Point>(new[] { start });
-
-            while (boundary.Any())
-            {
-                var current = boundary.Dequeue();
-                var currentHeight = heightMap[current.X, current.Y];
-                var currentDist = seen[current.X, current.Y].Value;
-
-                var toTarget = current.GetSurrounding4()
-                    .Where(it => it.WithinBounds(0, lines[0].Length - 1, 0, lines.Length - 1))
-                    .Where(it => seen[it.X, it.Y] == null)
-                    .Where(it => heightMap[it.X, it.Y] <= currentHeight + 1)
-                    .ToArray();
-
-                if (toTarget.Any(it => it.Equals(end)))
-                    return currentDist + 1;
-
-                foreach(var item in toTarget)
-                {
-                    seen[item.X, item.Y] = seen[current.X, current.Y] + 1;
-                    boundary.Enqueue(item);
-                }
-            }
-
-            throw new Exception("Unable to find path");
+            return (start, end, heightMap);
         }
 
-        [Solution(12, 2)]
-        public int Solution2(string input)
+        private int SearchMap(Point start, int[,] heightMap, Func< int, int, bool> validCheck, Func<Point, bool> completeCheck)
         {
-            var lines = Parser.ToArrayOfString(input);
-            var start = new Point();
-            var heightMap = new int[lines[0].Length, lines.Length];
-
-            for (var y = 0; y < lines.Length; y++)
-            {
-                var line = lines[y];
-                for (var x = 0; x < line.Length; x++)
-                {
-                    var item = line[x];
-                    if (item == 'S')
-                        heightMap[x, y] = 0;
-                    else if (item == 'E')
-                    {
-                        start = new Point(x, y);
-                        heightMap[x, y] = 25;
-                    }
-                    else
-                        heightMap[x, y] = item - 'a';
-                }
-            }
-
-            var seen = new int?[lines[0].Length, lines.Length];
+            var width = heightMap.GetLength(0);
+            var height = heightMap.GetLength(1);
+            var seen = new int?[width, height];
             seen[start.X, start.Y] = 0;
             var boundary = new Queue<Point>(new[] { start });
 
@@ -104,12 +71,12 @@ namespace AdventOfCode.Solutions._2022
                 var currentDist = seen[current.X, current.Y].Value;
 
                 var toTarget = current.GetSurrounding4()
-                    .Where(it => it.WithinBounds(0, lines[0].Length - 1, 0, lines.Length - 1))
+                    .Where(it => it.WithinBounds(0, width - 1, 0, height - 1))
                     .Where(it => seen[it.X, it.Y] == null)
-                    .Where(it => heightMap[it.X, it.Y] >= currentHeight - 1)
+                    .Where(it => validCheck.Invoke(heightMap[it.X, it.Y], currentHeight))
                     .ToArray();
 
-                if (toTarget.Any(it => heightMap[it.X, it.Y] == 0))
+                if (toTarget.Any(it => completeCheck.Invoke(it)))
                     return currentDist + 1;
 
                 foreach (var item in toTarget)
