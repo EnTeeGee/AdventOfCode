cb96d946196d465cc281173e241238b702e27587
EnTeeGee <enteegee@protonmail.com>
2022-12-27 23:33:57 +0000

    Add notes file and (slow) solution for 2016 day 11 part 2

--- AdventOfCode/Solutions/2016/Day11.cs
+++ AdventOfCode/Solutions/2016/Day11.cs
@@ -18,11 +18,12 @@ namespace AdventOfCode.Solutions._2016
             var dict = Enumerable.Range(1, 3).ToDictionary(it => it, it => Parser.SplitOn(lines[it - 1], ",", " and").Select(l => new Component(l)).ToArray());
             dict.Add(4, new Component[0]);
 
-            //return Run(dict);
-            return Run2(dict);
+            return Run(dict);
+            //return Run2(dict);
+            //return Run3(dict);
         }
 
-        //[Solution(11, 2)]
+        [Solution(11, 2)]
         public int Solution2(string input)
         {
             var lines = Parser.ToArrayOfString(input);
@@ -33,38 +34,53 @@ namespace AdventOfCode.Solutions._2016
             dict[1] = updatedFloor;
 
             //return Run(dict);
-            return Run2(dict);
+            //return Run2(dict);
+            return Run3(dict);
         }
 
         private int Run(Dictionary<int, Component[]> input)
         {
             var start = new State(0, input, 1);
-            var queue = new Queue<State>();
-            queue.Enqueue(start);
-            var seen = new HashSet<State>();
-            seen.Add(start);
+            //var queue = new Queue<State>();
+            //queue.Enqueue(start);
+            var queue = new LinkedList<State>();
+            queue.AddLast(start);
+            //var seen = new HashSet<State>();
+            var seen = new HashSet<string>();
+            seen.Add(start.Uid);
+
+            var currentStep = 0;
 
             while (queue.Any())
             {
-                var current = queue.Dequeue();
+                //var current = queue.Dequeue();
+                var current = queue.First();
+                queue.RemoveFirst();
+                if(current.Steps > currentStep)
+                {
+                    Console.WriteLine($"Updated steps to {current.Steps}, items in queue: {queue.Count}");
+                    currentStep = current.Steps;
+                }
                 if (current.IsFinished())
                 {
                     foreach (var item in current.PriorStates.Concat(new[] { current }))
                     {
-                        Console.WriteLine(item.Draw());
-                        Console.WriteLine();
+                        //Console.WriteLine(item.Draw());
+                        //Console.WriteLine();
+                        //Console.WriteLine(item.Uid);
                     }
 
                     return current.Steps;
                 }
 
 
-                var options = current.GetMoves().Where(it => !seen.Contains(it)).ToArray();
+                var options = current.GetMoves().Where(it => !seen.Contains(it.Uid)).ToArray();
                 foreach (var item in options)
                 {
-                    queue.Enqueue(item);
-                    seen.Add(item);
-                    item.PriorStates.AddRange(current.PriorStates.Concat(new[] { current }));
+                    //queue.Enqueue(item);
+                    queue.AddLast(item);
+                    seen.Add(item.Uid);
+                    //item.PriorStates.AddRange(current.PriorStates.Concat(new[] { current }));
                 }
             }
 
@@ -85,11 +101,11 @@ namespace AdventOfCode.Solutions._2016
                 //    return current.Steps;
                 if (current.IsFinished())
                 {
-                    foreach (var item in current.PriorStates.Concat(new[] { current }))
-                    {
-                        Console.WriteLine(item.Draw());
-                        Console.WriteLine();
-                    }
+                    //foreach (var item in current.PriorStates.Concat(new[] { current }))
+                    //{
+                    //    Console.WriteLine(item.Draw());
+                    //    Console.WriteLine();
+                    //}
 
                     return current.Steps;
                 }
@@ -104,6 +120,67 @@ namespace AdventOfCode.Solutions._2016
                     else
                         seen[item] = item.Steps;
                     item.PriorStates.AddRange(current.PriorStates.Concat(new[] { current }));
+                }
+            }
+
+            throw new Exception("Found no paths");
+        }
+
+        private int Run3(Dictionary<int, Component[]> input)
+        {
+            var start = new State(0, input, 1);
+            var endState = Enumerable.Range(1, 4).ToDictionary(it => it, it => new Component[0]);
+            endState[4] = input.SelectMany(it => it.Value).ToArray();
+            var end = new State(0, endState, 4);
+            var startQueue = new Queue<State>();
+            var endQueue = new Queue<State>();
+            startQueue.Enqueue(start);
+            endQueue.Enqueue(end);
+            //var startSeen = new HashSet<State> { start };
+            //var endSeen = new HashSet<State> { end };
+            var startSeen = new Dictionary<string, int> { { start.Uid, 0 } };
+            var endSeen = new Dictionary<string, int> { { end.Uid, 0 } };
+
+            var currentStep = 0;
+
+            while (startQueue.Any() && endQueue.Any())
+            {
+                var currentStart = startQueue.Dequeue();
+                if (currentStart.Steps > currentStep)
+                {
+                    Console.WriteLine($"Updated steps to {currentStart.Steps}, items in queue: {startQueue.Count}");
+                    currentStep = currentStart.Steps;
+                }
+
+                if (endSeen.ContainsKey(currentStart.Uid))
+                {
+                    //endSeen.TryGetValue(currentStart, out var matching);
+                    var matching = endSeen[currentStart.Uid];
+
+                    return currentStart.Steps + matching;
+                }
+
+                var startOptions = currentStart.GetMoves().Where(it => !startSeen.ContainsKey(it.Uid)).ToArray();
+                foreach(var item in startOptions)
+                {
+                    startQueue.Enqueue(item);
+                    startSeen.Add(item.Uid, item.Steps);
+                }
+
+                var currentEnd = endQueue.Dequeue();
+                if (startSeen.ContainsKey(currentEnd.Uid))
+                {
+                    //startSeen.TryGetValue(currentEnd, out var matching);
+                    var matching = startSeen[currentEnd.Uid];
+
+                    return currentEnd.Steps + matching;
+                }
+
+                var endOptions = currentEnd.GetMoves(true).Where(it => !endSeen.ContainsKey(it.Uid)).ToArray();
+                foreach(var item in endOptions)
+                {
+                    endQueue.Enqueue(item);
+                    endSeen.Add(item.Uid, item.Steps);
                 }
             }
 
@@ -129,7 +206,7 @@ namespace AdventOfCode.Solutions._2016
 
             public override string ToString()
             {
-                return $"{Kind}-{(IsGenerator ? "gen" : "chip")}";
+                return $"{Kind.Substring(0, 2)}-{(IsGenerator ? "G" : "M")}";
             }
         }
 
@@ -144,6 +221,8 @@ namespace AdventOfCode.Solutions._2016
             public string Uid { get; }
             public int DistanceToEnd { get; }
 
+            private int hash;
+
             public State(int steps, Dictionary<int, Component[]> onFloors, int liftFloor)
             {
                 Steps = steps;
@@ -151,11 +230,13 @@ namespace AdventOfCode.Solutions._2016
                 LiftFloor = liftFloor;
 
                 Uid = $"F:{LiftFloor}|I:{DictToString()}";
-                DistanceToEnd = OnFloors.Select(it => (4 - it.Key) * it.Value.Length).Sum();
+                //DistanceToEnd = OnFloors.Select(it => (4 - it.Key) * it.Value.Length).Sum();
+                //DistanceToEnd = OnFloors.Select(it => (4 - it.Key) * it.Value.Length).Sum() + steps;
                 PriorStates = new List<State>();
+                hash = Uid.GetHashCode();
             }
 
-            public State[] GetMoves()
+            public State[] GetMoves(bool reversed = false)
             {
                 var itemsOnFloor = OnFloors[LiftFloor];
                 var floorsToMoveTo = new[] { LiftFloor - 1, LiftFloor + 1 }.Where(it => OnFloors.ContainsKey(it)).ToArray();
@@ -166,7 +247,20 @@ namespace AdventOfCode.Solutions._2016
 
                 foreach(var floor in floorsToMoveTo)
                 {
-                    foreach(var move in possibleMoves)
+                    //var listToUse = new Component[0][];
+                    //if (floor == LiftFloor + 1)
+                    //    listToUse = possibleMoves;
+                    //else
+                    //    listToUse = singleItems.ToArray();
+                    //foreach(var move in listToUse)
+
+                    //var altFloor = reversed ? LiftFloor - 1 : LiftFloor + 1;
+                    //if(floor == altFloor)
+                    //{
+                    //    possibleMoves = possibleMoves.Where(it => !it.Any(c => c.IsGenerator)).ToArray();
+                    //}
+
+                    foreach (var move in possibleMoves)
                     {
                         var newDict = new Dictionary<int, Component[]>();
                         foreach(var target in OnFloors.Keys)
@@ -181,6 +275,11 @@ namespace AdventOfCode.Solutions._2016
 
                         output.Add(new State(Steps + 1, newDict, floor));
                     }
+                }
+                
+                foreach(var item in output)
+                {
+                    var test = item.IsValidState();
                 }
 
                 return output.Where(it => it.IsValidState()).ToArray();
@@ -237,7 +336,8 @@ namespace AdventOfCode.Solutions._2016
 
             public override int GetHashCode()
             {
-                return Uid.GetHashCode();
+                //return Uid.GetHashCode();
+                return hash;
             }
 
             public override bool Equals(object obj)
