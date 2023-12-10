df9fcc4f6333bb16a57dff3e6023f3d047d87550
EnTeeGee <enteegee@protonmail.com>
2023-12-10 20:34:10 +0000

    Add method to report on the rate people are getting filtered

---
+++ AdventOfCodeCore/Core/FilterRate.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCodeCore.Common;
+
+namespace AdventOfCodeCore.Core
+{
+    internal class FilterRate : IRunner
+    {
+        public bool CheckRequest(string[] args)
+        {
+            if (args.Length == 0 || args[0].ToLower() != "filtered")
+                return false;
+
+            Compute();
+
+            return true;
+        }
+
+        private void Compute()
+        {
+            var input = Clipboard.Read();
+            if (input == null)
+                return;
+
+            var lines = Parser.ToArrayOf(input, it => Parser.SplitOnSpace(it).Take(3).ToArray()).Reverse().Where(it => it[1] != "0").ToArray();
+            var results = lines.Zip(lines.Skip(1), (a, b) => ComputeItem(a[1], b[1])).ToArray();
+
+            var average = results.Sum() / results.Length;
+
+            var startValue = int.Parse(lines[0][1]);
+            var test = startValue * Math.Pow(average, results.Length);
+            var test2 = startValue * Math.Pow(average, 24);
+
+            var message = $"So far about {(1 - average).ToString("P0")} of people are filtered each day.{Environment.NewLine}" +
+                $"If it continues at this rate, approximately {(int)test2} people will manage to get all 50 stars and save Christmas.";
+
+            Console.WriteLine(message);
+            Clipboard.Write(message);
+        }
+        
+        private double ComputeItem(string beforeStr, string afterStr)
+        {
+            var before = int.Parse(beforeStr);
+            var after = int.Parse(afterStr);
+            return 1 - ((before - after) / (double)before);
+        }
+
+        public void PrintStartupMessage() { }
+    }
+}
--- AdventOfCodeCore/Program.cs
+++ AdventOfCodeCore/Program.cs
@@ -2,10 +2,11 @@
 
 var yearManager = new YearManager();
 var solutionRunner = new SolutionRunner(yearManager);
+var filterRate = new FilterRate();
 //var rendererRunner = new RendererRunner(yearManager);
 
 //var runners = new IRunner[] { solutionRunner, rendererRunner, yearManager };
-var runners = new IRunner[] { solutionRunner, yearManager };
+var runners = new IRunner[] { solutionRunner, yearManager, filterRate };
 
 foreach (var item in runners)
     item.PrintStartupMessage();
