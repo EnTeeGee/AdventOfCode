513df260180de9d0d3c7794e6ee3d3fcef4e5889
EnTeeGee <enteegee@protonmail.com>
2023-12-13 12:59:47 +0000

    Solutions for day 13

---
+++ AdventOfCodeCore/Solutions/2023/Day13.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCodeCore.Common;
+using AdventOfCodeCore.Core;
+
+namespace AdventOfCodeCore.Solutions._2023
+{
+    internal class Day13
+    {
+        [Solution(13, 1)]
+        public int Solution1(string input)
+        {
+            return Parser.ToArrayOfGroups(input)
+                .Select(it => Parser.ToArrayOfString(it))
+                .Select(it => FindReflection(it) ?? (100 * FindReflection(RotateMap(it))))
+                .Sum()
+                !.Value;
+        }
+
+        [Solution(13, 2)]
+        public int Solution2(string input)
+        {
+            return Parser.ToArrayOfGroups(input)
+                .Select(it => Parser.ToArrayOfString(it))
+                .Select(it => FindSmudged(it) ?? (100 * FindSmudged(RotateMap(it))))
+                .Sum()
+                !.Value;
+        }
+
+        private string[] RotateMap(string[] input)
+        {
+            return Enumerable.Range(0, input[0].Length)
+                .Select(it => new string(input.Select(l => l[it]).ToArray()))
+                .Reverse()
+                .ToArray();
+        }
+
+        private int? FindReflection(string[] map)
+        {
+            return Enumerable.Range(1, map[0].Length - 1)
+                .Cast<int?>()
+                .FirstOrDefault(it => map
+                    .Select(l => l[..it!.Value].Reverse()
+                        .Zip(l[it.Value..], (a, b) => a == b)
+                        .All(c => c))
+                    .All(l => l));
+        }
+
+        private int? FindSmudged(string[] map)
+        {
+            return Enumerable.Range(1, map[0].Length - 1)
+                .Cast<int?>()
+                .FirstOrDefault(it => map
+                    .Select(l => l[..it!.Value].Reverse()
+                        .Zip(l[it.Value..], (a, b) => a == b ? 0 : 1)
+                        .Sum())
+                    .Sum() == 1);
+        }
+    }
+}
