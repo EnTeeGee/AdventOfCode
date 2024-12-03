dddeb72903207f3f1d4541a71e5f8eddcf36c89b
EnTeeGee <enteegee@protonmail.com>
2024-12-03 10:35:13 +0000

    Solutions for day 3

---
+++ AdventOfCodeCore/Solutions/2024/Day03.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCodeCore.Common;
+using AdventOfCodeCore.Core;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCodeCore.Solutions._2024
+{
+    internal class Day03
+    {
+        [Solution(3, 1)]
+        public int Solution1(string input)
+        {
+            return Regex.Matches(input, "mul\\(\\d+,\\d+\\)")
+                .Select(it => Parser.SplitOn(it.Value, '(', ')', ','))
+                .Sum(it => int.Parse(it[1]) * int.Parse(it[2]));
+        }
+
+        [Solution(3, 2)]
+        public int Solution2(string input)
+        {
+            var dos = new[] { 0 }.Concat(Regex.Matches(input, "do\\(\\)").Select(it => it.Index)).ToArray();
+            var donts = Regex.Matches(input, "don\\'t\\(\\)").Select(it => it.Index).Concat(new[] { input.Length }).ToArray();
+
+            var ranges = dos
+                .Select(it => new { start = it, end = donts.FirstOrDefault(it2 => it2 > it) })
+                .ToArray();
+
+            return Regex.Matches(input, "mul\\(\\d+,\\d+\\)")
+                .Where(it => ranges.Any(r => r.start < it.Index && r.end > it.Index))
+                .Select(it => Parser.SplitOn(it.Value, '(', ')', ','))
+                .Sum(it => int.Parse(it[1]) * int.Parse(it[2]));
+        }
+    }
+}
